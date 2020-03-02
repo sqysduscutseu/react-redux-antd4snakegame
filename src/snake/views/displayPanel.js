728c2b6e4cc2c8cb9f114d2594ba1b429a18bb9b
Kevin Shek <sqysduscut.seu@foxmail.com>
2020-03-02 23:07:30 +0800

    小屏样式微调

--- src/snake/views/displayPanel.js
+++ src/snake/views/displayPanel.js
@@ -6,21 +6,21 @@ const DisplayPanel = (props) => {
     return(
         <div className = 'displayPanel-wrap'>
             <Row>
-                <Col md = {} lg = {4} xl = {6}></Col>
-                <Col className = 'btn-lay' md = {} lg = {4} xl = {3}>
+                <Col md = {1} lg = {4} xl = {6}></Col>
+                <Col className = 'btn-lay' md = {5} lg = {4} xl = {3}>
                     <Button type = 'primary' size = 'large' onClick = {gameStart} disabled = {isButtonDisabled} ><Icon type="play-circle" />START</Button>
                 </Col>
-                <Col className = 'radio-lay' lg = {6} xl = {5}>
+                <Col className = 'radio-lay' md = {12} lg = {6} xl = {5}>
                     <Radio.Group defaultValue = 'n' size = 'large' buttonStyle = 'solid' disabled = {isButtonDisabled}>
                         <Radio.Button value = 's' onClick = {() => {handleSpeed(80)}}>蜗牛蛇</Radio.Button>
                         <Radio.Button value = 'n' onClick = {() => {handleSpeed(50)}}>草蛇</Radio.Button>
                         <Radio.Button value = 'f' onClick = {() => {handleSpeed(20)}}>闪电蛇</Radio.Button>
                     </Radio.Group>
                 </Col>
-                <Col className = 'score-lay' lg = {8} xl = {4}>
+                <Col className = 'score-lay' md = {4} lg = {8} xl = {4}>
                     <Statistic title = '你吃了' value = {`${score}坨`} />
                 </Col>
-                <Col lg = {0} xl = {5}></Col>
+                <Col md = {2} lg = {0} xl = {5}></Col>
             </Row> 
         </div>
     )
