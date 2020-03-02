a0ee5a5bb464a0954402016a6c3296575b4f6dd0
Kevin Shek <sqysduscut.seu@foxmail.com>
2020-03-02 21:52:23 +0800

    按键响应式调整~
    Signed-off-by: Kevin Shek <sqysduscut.seu@foxmail.com>

--- src/snake/views/displayPanel.js
+++ src/snake/views/displayPanel.js
@@ -6,20 +6,21 @@ const DisplayPanel = (props) => {
     return(
         <div className = 'displayPanel-wrap'>
             <Row>
-                <Col span = {4}></Col>
-                <Col className = 'btn-lay' span = {4}>
+                <Col md = {} lg = {4} xl = {6}></Col>
+                <Col className = 'btn-lay' md = {} lg = {4} xl = {3}>
                     <Button type = 'primary' size = 'large' onClick = {gameStart} disabled = {isButtonDisabled} ><Icon type="play-circle" />START</Button>
                 </Col>
-                <Col className = 'radio-lay' span = {6}>
+                <Col className = 'radio-lay' lg = {6} xl = {5}>
                     <Radio.Group defaultValue = 'n' size = 'large' buttonStyle = 'solid' disabled = {isButtonDisabled}>
                         <Radio.Button value = 's' onClick = {() => {handleSpeed(80)}}>蜗牛蛇</Radio.Button>
                         <Radio.Button value = 'n' onClick = {() => {handleSpeed(50)}}>草蛇</Radio.Button>
                         <Radio.Button value = 'f' onClick = {() => {handleSpeed(20)}}>闪电蛇</Radio.Button>
                     </Radio.Group>
                 </Col>
-                <Col className = 'score-lay' span = {8}>
+                <Col className = 'score-lay' lg = {8} xl = {4}>
                     <Statistic title = '你吃了' value = {`${score}坨`} />
                 </Col>
+                <Col lg = {0} xl = {5}></Col>
             </Row> 
         </div>
     )
